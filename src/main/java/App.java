2c58fda8091f8dba8d02246d4d05d61820f1ff3c
revanth-6 <307519@ust.com>
2026-04-07 19:43:12 +0530

    11th commit

---
+++ src/main/java/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello CI");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
